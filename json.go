aef607488ceac9db14d85e00097eedd46b0dd006
James Xu <xumingmingv@gmail.com>
2014-11-11 00:12:45 +0800

    enhance number

--- json.go
+++ json.go
@@ -1,250 +1,305 @@
 package gojson
 
 import (
-    "bytes"
-    "fmt"
-    "strconv"
+	"bytes"
+	"fmt"
+	"strconv"
 )
 
+type JSONObject struct {
+	pairs map[string]interface{}
+}
+
+type JSONArray struct {
+	values []interface{}
+}
+
+type Number struct {
+	data     string
+	negative bool
+	isFloat  bool
+}
+
+func NewNumber(data string, negative bool, isFloat bool) Number {
+	var ret Number
+	ret.data = data
+	ret.negative = negative
+	ret.isFloat = isFloat
+
+	return ret
+}
+
+func (n Number) Float64() float64 {
+	f, _ := strconv.ParseFloat(n.data, 0)
+	if n.negative {
+		f = 0 - f
+	}
+
+	return f
+}
+
+func (n Number) Int64() int64 {
+	i, _ := strconv.ParseInt(n.data, 10, 0)
+	if n.negative {
+		i = 0 - i
+	}
+
+	return i
+}
+
 type Lexer struct {
-    /* a very good string */
-    content string
-    index   int
-    char    uint8
+	/* a very good string */
+	content string
+	index   int
+	char    uint8
 }
 
 func NewLexer(content string) *Lexer {
-    lexer := new(Lexer)
-    lexer.content = content
-    lexer.index = -1
-    lexer.char = 0
-    lexer.nextChar()
+	lexer := new(Lexer)
+	lexer.content = content
+	lexer.index = -1
+	lexer.char = 0
+	lexer.nextChar()
 
-    return lexer
+	return lexer
 }
 
 func (this *Lexer) match(x uint8) bool {
-    return this.char == x
+	return this.char == x
 }
 
 func (this *Lexer) accept(x uint8) {
-    if this.match(x) {
-        this.nextChar()
-    } else {
-        panic(fmt.Sprintf("expecting '%v', got '%v'[index: %v]", string(x), string(this.char), this.index))
-    }
+	if this.match(x) {
+		this.nextChar()
+	} else {
+		panic(fmt.Sprintf("expecting '%v', got '%v'[index: %v]", string(x), string(this.char), this.index))
+	}
 }
 
 func (this *Lexer) nextChar() uint8 {
-    this.index += 1
-    if this.index < len(this.content) {
-        this.char = this.content[this.index]
-    } else {
-        this.char = 0
-    }
+	this.index += 1
+	if this.index < len(this.content) {
+		this.char = this.content[this.index]
+	} else {
+		this.char = 0
+	}
+
+	return this.char
+}
+
+func (this *Lexer) prevChar() uint8 {
+	this.index -= 1
+	if this.index > -1 {
+		this.char = this.content[this.index]
+	} else {
+		this.char = 0
+	}
 
 	return this.char
 }
 
 func (this *Lexer) readString() string {
-    var ret bytes.Buffer
-    this.accept('"')
-    escaping := false
-    for !this.match('"') || escaping {
-        if this.match('\\') {
-            escaping = true
-            this.nextChar()
-            continue
-        }
+	var ret bytes.Buffer
+	this.accept('"')
+	escaping := false
+	for !this.match('"') || escaping {
+		if this.match('\\') {
+			escaping = true
+			this.nextChar()
+			continue
+		}
 
-        var actualChar uint8
-        actualChar = this.char
-        if escaping {
-            switch this.char {
-            case '\\':
-                actualChar = '\\'
-                ret.WriteByte(actualChar)
-            case 'b':
-                actualChar = '\b'
-                ret.WriteByte(actualChar)               
-            case 'f':
-                actualChar = '\f'
-                ret.WriteByte(actualChar)               
-            case 'n':
-                actualChar = '\n'
-                ret.WriteByte(actualChar)               
-            case 'r':
-                actualChar = '\r'
-                ret.WriteByte(actualChar)               
-            case 't':
-                actualChar = '\t'
-                ret.WriteByte(actualChar)               
-            case '"':
-                actualChar = '"'
-                ret.WriteByte(actualChar)               
-            case 'u':
-                i32 := int32(0)
-                for i := 0; i < 4; i++ {
-                    i32 *= 16
-                    this.nextChar()
-                    if this.char <= '9' {
-                        i32 += int32(this.char - '0')
-                    } else {
-                        i32 += int32(this.char - 'a' + 10)
-                    }
-                }
-                ret.WriteRune(i32)              
-            }
+		var actualChar uint8
+		actualChar = this.char
+		if escaping {
+			switch this.char {
+			case '\\':
+				actualChar = '\\'
+				ret.WriteByte(actualChar)
+			case 'b':
+				actualChar = '\b'
+				ret.WriteByte(actualChar)
+			case 'f':
+				actualChar = '\f'
+				ret.WriteByte(actualChar)
+			case 'n':
+				actualChar = '\n'
+				ret.WriteByte(actualChar)
+			case 'r':
+				actualChar = '\r'
+				ret.WriteByte(actualChar)
+			case 't':
+				actualChar = '\t'
+				ret.WriteByte(actualChar)
+			case '"':
+				actualChar = '"'
+				ret.WriteByte(actualChar)
+			case 'u':
+				i32 := int32(0)
+				for i := 0; i < 4; i++ {
+					i32 *= 16
+					this.nextChar()
+					if this.char <= '9' {
+						i32 += int32(this.char - '0')
+					} else {
+						i32 += int32(this.char - 'a' + 10)
+					}
+				}
+				ret.WriteRune(i32)
+			}
 
-            escaping = false
-        } else {
-            ret.WriteByte(actualChar)
-        }
-        
-        this.nextChar()
-    }
+			escaping = false
+		} else {
+			ret.WriteByte(actualChar)
+		}
 
-    this.accept('"')
-    return ret.String()
+		this.nextChar()
+	}
+
+	this.accept('"')
+	return ret.String()
 }
 
-func (this *Lexer) readInt() int {
-    var ret bytes.Buffer
+func (this *Lexer) readNumber() Number {
+	var ret bytes.Buffer
 	negative := false
+	isFloat := false
 	if this.match('-') {
 		negative = true
 		this.accept('-')
 	}
-	
-    for '0' <= this.char && this.char <= '9' {
-        ret.WriteByte(this.char)
-        this.nextChar()
-    }
 
-    i, error := strconv.Atoi(ret.String())
-    if error != nil {
-        fmt.Printf("the error is: %v", error)
-    }
-	if negative {
-		i -= 2 * i
+	for '0' <= this.char && this.char <= '9' {
+		ret.WriteByte(this.char)
+		this.nextChar()
 	}
-	
-    return i
+
+	if this.match('.') {
+		this.nextChar()
+		if isDigit(this.char) {
+			isFloat = true
+			ret.WriteByte('.')
+			for '0' <= this.char && this.char <= '9' {
+				ret.WriteByte(this.char)
+				this.nextChar()
+			}
+		} else {
+			panic("unexpected symbol: '.'")
+		}
+	}
+
+	return NewNumber(ret.String(), negative, isFloat)
 }
 
 func (this *Lexer) readBoolean() bool {
-    var ret bytes.Buffer
-    if this.match('t') || this.match('f') {
-        //      for this.char != ',' && this.char != ' ' && this.char != '}' {
-        for this.char != ',' && !isBlank(this.char) && this.char != '}' && this.char != 0 {
-            ret.WriteByte(this.char)
-            this.nextChar()
-        }
-    }
+	var ret bytes.Buffer
+	if this.match('t') || this.match('f') {
+		//      for this.char != ',' && this.char != ' ' && this.char != '}' {
+		for this.char != ',' && !isBlank(this.char) && this.char != '}' && this.char != 0 {
+			ret.WriteByte(this.char)
+			this.nextChar()
+		}
+	}
 
-    if ret.String() == "true" {
-        return true
-    } else {
-        return false
-    }
+	if ret.String() == "true" {
+		return true
+	} else {
+		return false
+	}
 }
 
 func (this *Lexer) readObject() JSONObject {
-    this.skipBlank()
-    this.accept('{')
-    var ret JSONObject
-    ret.pairs = map[string]interface{}{}
-    name, value := this.readPair()
-    ret.pairs[name] = value
+	this.skipBlank()
+	this.accept('{')
+	var ret JSONObject
+	ret.pairs = map[string]interface{}{}
+	name, value := this.readPair()
+	ret.pairs[name] = value
 
-    for this.match(',') {
-        this.accept(',')
-        name, value = this.readPair()
-        ret.pairs[name] = value
-    }
-    this.skipBlank()
-    this.accept('}')
+	for this.match(',') {
+		this.accept(',')
+		name, value = this.readPair()
+		ret.pairs[name] = value
+	}
+	this.skipBlank()
+	this.accept('}')
 
-    return ret
+	return ret
 }
 
 func (this *Lexer) readArray() JSONArray {
-    this.accept('[')
-    var ret JSONArray
-    var values []interface{}
+	this.accept('[')
+	var ret JSONArray
+	var values []interface{}
 
-    value := this.readValue()
-    values = append(values, value)
-    for this.match(',') {
-        this.accept(',')
-        value = this.readValue()
-        values = append(values, value)
-    }
-    this.accept(']')
-    ret.values = values
-    return ret
+	value := this.readValue()
+	values = append(values, value)
+	for this.match(',') {
+		this.accept(',')
+		value = this.readValue()
+		values = append(values, value)
+	}
+	this.accept(']')
+	ret.values = values
+	return ret
 }
 
 //
 func (this *Lexer) readNull() {
-    this.nextChar()
-    this.nextChar()
-    this.nextChar()
+	this.nextChar()
+	this.nextChar()
+	this.nextChar()
 }
 
 //
 func (this *Lexer) readValue() (value interface{}) {
-    if this.match('"') {
-        value = this.readString()
-    } else if '0' <= this.char && this.char <= '9' || this.match('-'){
-        value = this.readInt()
-    } else if this.match('t') || this.match('f') {
-        value = this.readBoolean()
-    } else if this.match('{') {
-        value = this.readObject()
-    } else if this.match('[') {
-        value = this.readArray()
-    } else {
-        value = nil
-        this.readNull()
-    }
+	if this.match('"') {
+		value = this.readString()
+	} else if '0' <= this.char && this.char <= '9' || this.match('-') {
+		value = this.readNumber()
+	} else if this.match('t') || this.match('f') {
+		value = this.readBoolean()
+	} else if this.match('{') {
+		value = this.readObject()
+	} else if this.match('[') {
+		value = this.readArray()
+	} else {
+		value = nil
+		this.readNull()
+	}
 
-    return value
+	return value
 }
 
 func (this *Lexer) readPair() (name string, value interface{}) {
-    name = this.readString()
-    this.skipBlank()
-    this.accept(':')
-    this.skipBlank()
-    value = this.readValue()
-    return
+	name = this.readString()
+	this.skipBlank()
+	this.accept(':')
+	this.skipBlank()
+	value = this.readValue()
+	return
 }
 
 func (this *Lexer) skipBlank() {
-    for isBlank(this.char) {
-        this.nextChar()
-    }
+	for isBlank(this.char) {
+		this.nextChar()
+	}
 }
 
 func isBlank(x uint8) bool {
-    return x == ' ' || x == '\t' || x == '\r' || x == '\n'
+	return x == ' ' || x == '\t' || x == '\r' || x == '\n'
+}
+
+func isDigit(x uint8) bool {
+	return x >= '0' && x <= '9'
 }
 
 func Parse(str string) interface{} {
-    lexer := NewLexer(str)
-    if lexer.match('{') {
-        return lexer.readObject()
-    } else if lexer.match('[') {
-        return lexer.readArray()
-    }
-    return nil
-}
-
-type JSONObject struct {
-    pairs map[string]interface{}
-}
-
-type JSONArray struct {
-    values []interface{}
+	lexer := NewLexer(str)
+	if lexer.match('{') {
+		return lexer.readObject()
+	} else if lexer.match('[') {
+		return lexer.readArray()
+	}
+	return nil
 }
--- json_test.go
+++ json_test.go
@@ -1,87 +1,94 @@
 package gojson
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestReadInt(t *testing.T) {
-    var lexer *Lexer
-    var i int
-    lexer = NewLexer("1234567")
-    i = lexer.readInt()
-    if i != 1234567 {
-        t.Fail()
-    }
+	var lexer *Lexer
+	var n Number
+	lexer = NewLexer("1234567")
+	n = lexer.readNumber()
+	if n.isFloat || n.Int64() != 1234567 {
+		t.Fail()
+	}
 
-    // test a big int(larger than 2 ^ 32)
-    lexer = NewLexer("8589934592")
-    i = lexer.readInt()
-    if i != 8589934592 {
-        t.Fail()
-    }
+	// test a big int(larger than 2 ^ 32)
+	lexer = NewLexer("8589934592")
+	n = lexer.readNumber()
+	if n.isFloat || n.Int64() != 8589934592 {
+		t.Fail()
+	}
 
 	// test negative number
-    lexer = NewLexer("-100")
-    i = lexer.readInt()
-    if i != -100 {
-        t.Fail()
-    }
+	lexer = NewLexer("-100")
+	n = lexer.readNumber()
+	if n.isFloat || n.Int64() != -100 {
+		t.Fail()
+	}
+
+	// test float number
+	lexer = NewLexer("-123.456")
+	n = lexer.readNumber()
+	if !n.isFloat || n.Float64() != -123.456 {
+		t.Fail()
+	}
 }
 
 func TestReadString(t *testing.T) {
-    var lexer *Lexer
-    var str string
+	var lexer *Lexer
+	var str string
 
-    // test normal case
-    lexer = NewLexer(`"hello"`)
-    str = lexer.readString()
-    if str != "hello" {
-        t.Fail()
-    }
+	// test normal case
+	lexer = NewLexer(`"hello"`)
+	str = lexer.readString()
+	if str != "hello" {
+		t.Fail()
+	}
 
-    // test \n
-    lexer = NewLexer(`"hello world\n"`)
-    str = lexer.readString()
-    if str != "hello world\n" {
-        t.Fail()
-    }
+	// test \n
+	lexer = NewLexer(`"hello world\n"`)
+	str = lexer.readString()
+	if str != "hello world\n" {
+		t.Fail()
+	}
 
-    // test \"
-    lexer = NewLexer(`"hello\""`)
-    str = lexer.readString()
-    if str != "hello\"" {
-        t.Fail()
-    }
+	// test \"
+	lexer = NewLexer(`"hello\""`)
+	str = lexer.readString()
+	if str != "hello\"" {
+		t.Fail()
+	}
 
-    // test unicode
-    lexer = NewLexer(`"你好\u554a"`)
-    str = lexer.readString()
-    if str != "你好啊" {
-        t.Fail()
-    }
+	// test unicode
+	lexer = NewLexer(`"你好\u554a"`)
+	str = lexer.readString()
+	if str != "你好啊" {
+		t.Fail()
+	}
 }
 
 func TestReadBoolean(t *testing.T) {
-    var lexer *Lexer
-    var ret bool
-    
-    lexer = NewLexer(`true`)
-    ret = lexer.readBoolean()
-    if ret != true {
-        t.Fail()
-    }
+	var lexer *Lexer
+	var ret bool
 
-    lexer = NewLexer(`false`)
-    ret = lexer.readBoolean()
-    if ret != false {
-        t.Fail()
-    }
-    
-    lexer = NewLexer(`true1`)
-    ret = lexer.readBoolean()
-    if ret == true {
-        t.Fail()
-    }
+	lexer = NewLexer(`true`)
+	ret = lexer.readBoolean()
+	if ret != true {
+		t.Fail()
+	}
+
+	lexer = NewLexer(`false`)
+	ret = lexer.readBoolean()
+	if ret != false {
+		t.Fail()
+	}
+
+	lexer = NewLexer(`true1`)
+	ret = lexer.readBoolean()
+	if ret == true {
+		t.Fail()
+	}
 }
 
 func TestReadNil(t *testing.T) {
@@ -97,62 +104,62 @@ func TestReadPair(t *testing.T) {
 }
 
 func TestReadObject(t *testing.T) {
-    var lexer *Lexer
-    var ret JSONObject
-    
-    lexer = NewLexer(`{"a": 149,"b":false,"c":"hello" }`)
-    ret = lexer.readObject()
-    if len(ret.pairs) != 3 {
-        t.Fail()
-    }
-    
-    if ret.pairs["a"] != 149 {
-        t.Fail()
-    }
+	var lexer *Lexer
+	var ret JSONObject
 
-    if ret.pairs["b"] != false {
-        t.Fail()
-    }
+	lexer = NewLexer(`{"a": 149,"b":false,"c":"hello" }`)
+	ret = lexer.readObject()
+	if len(ret.pairs) != 3 {
+		t.Fail()
+	}
 
-    if ret.pairs["c"] != "hello" {
-        t.Fail()
-    }
+	if ((ret.pairs["a"]).(Number)).Int64() != 149 {
+		t.Fail()
+	}
 
-    // test leading and trailing blanks
-    lexer = NewLexer(` 
+	if ret.pairs["b"] != false {
+		t.Fail()
+	}
+
+	if ret.pairs["c"] != "hello" {
+		t.Fail()
+	}
+
+	// test leading and trailing blanks
+	lexer = NewLexer(` 
                       {"a": 149} `)
-    ret = lexer.readObject()
-    if len(ret.pairs) != 1 {
-        t.Fail()
-    }
-    // for name, value := range ret.pairs {
-    //  fmt.Println(name, ": ", value)
-    // }
+	ret = lexer.readObject()
+	if len(ret.pairs) != 1 {
+		t.Fail()
+	}
+	// for name, value := range ret.pairs {
+	//  fmt.Println(name, ": ", value)
+	// }
 }
 
 func TestReadNestedObject(t *testing.T) {
-    lexer := NewLexer(`{"a":149,"b":false,"c":"hello","d":[1,2,"foo"],"e":{"hello":"world"}}`)
-    ret := lexer.readObject()
-    if &ret == nil {
-        t.Fail()
-    }
-    // for name, value := range ret.pairs {
-    //  fmt.Println(name, ": ", value)
-    // }
+	lexer := NewLexer(`{"a":149,"b":false,"c":"hello","d":[1,2,"foo"],"e":{"hello":"world"}}`)
+	ret := lexer.readObject()
+	if &ret == nil {
+		t.Fail()
+	}
+	// for name, value := range ret.pairs {
+	//  fmt.Println(name, ": ", value)
+	// }
 }
 
 func TestReadArray(t *testing.T) {
-    lexer := NewLexer(`[1,2,"hello"]`)
-    ret := lexer.readArray()
-    if &ret == nil {
-        t.Fail()
-    }
+	lexer := NewLexer(`[1,2,"hello"]`)
+	ret := lexer.readArray()
+	if &ret == nil {
+		t.Fail()
+	}
 }
 
 func TestParse(t *testing.T) {
-    ret := Parse(`{"a":149,"b":false,"c":"hello","d":[1,2,"foo"],"e":{"hello":"world"}}`)
-    ret = Parse(`[1,false,"hello"]`)
-    if &ret == nil {
-        t.Fail()
-    }
+	ret := Parse(`{"a":149,"b":false,"c":"hello","d":[1,2,"foo"],"e":{"hello":"world"}}`)
+	ret = Parse(`[1,false,"hello"]`)
+	if &ret == nil {
+		t.Fail()
+	}
 }
